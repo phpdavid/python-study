05dd557a6c0ed3cd6ebc8228e9e69eeb61d1afe1
david <785687000@qq.com>
2019-11-09 16:06:11 +0800

    类变量和类方法

--- home-study/study-code/python-code/five/one.py
+++ home-study/study-code/python-code/five/one.py
@@ -1,19 +1,28 @@
 # 学生类
 class Student():
-    #类变量和类的具体属性无关
-    name = ''
-    age = 0
+    # 类变量和类的具体属性无关
+    name = 'xiaoming'  # 姓名，年龄是个体的属性，不是群体的属性，不建议
+    age = 10
+    # 学生总数
+    sum = 0
 
     def __init__(self, name, age):
         # 初始化对象的属性
-        self.name = name #实例变量
+        self.name = name  # 实例变量
         self.age = age
+        # self.__class__.sum+=1 #操作类变量加一操作
+        # print(self.__class__.sum)
 
+    # 定义类方法
+    @classmethod  # 装饰器
+    def plus_count(cls):  # 主要用于操作类变量的操作
+        cls.sum += 1
+        print(cls.sum)
+
+    # 定义实例方法
     def speak(self):
-        print("my name is " + name + "i am " + str(age) + " years old")
-
-# def speak(self):
-#     print("my name is " + name + "i am " + str(age) + "years old")
+        print("my 类 name is " + __class__.name + " i am " + str(__class__.age) + " years old")
+        print("my 对象 name is " + self.name + " i am " + str(self.age) + " years old")
 
 # class Printer():
 #     def print_file(self):  # 这里必须要加self
