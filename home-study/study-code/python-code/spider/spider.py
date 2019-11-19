9500d5c60733afd31711c6a410a2253231c03a66
david <785687000@qq.com>
2019-11-19 19:32:08 +0800

    爬虫小试

--- home-study/study-code/python-code/spider/spider.py
+++ home-study/study-code/python-code/spider/spider.py
@@ -1,18 +1,25 @@
+import re
 from urllib import request
 
 
 class Spider():
-    url = "http://clot-test-erp.cqjlt.net/jlt-org.roles"
+    url = "https://music.douban.com/"
+    root_patten = '<div class="player-round-btn-bg">[\s\S]*?</div>'
 
     def __fetch_contents(self):
         r = request.urlopen(Spider.url)
         html = r.read()
-        a = 1
+        html = str(html, encoding='utf-8')
+        return html
+        # a=1
 
+    def __analysiy(self, html):
+        root_html = re.findall(Spider.root_patten, html)
+        print(root_html[0])
 
     def go(self):
-        r = self.__fetch_contents()
-
+        html = self.__fetch_contents()
+        self.__analysiy(html)
 
 
 result = Spider()
