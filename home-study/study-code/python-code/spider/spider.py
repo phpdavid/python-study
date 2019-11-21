e2d5da1fefe8cb8147c9d7857c41e678fdd2f775
david <785687000@qq.com>
2019-11-21 10:59:26 +0800

    爬http://jewelry.xbiao.com/

--- home-study/study-code/python-code/spider/spider.py
+++ home-study/study-code/python-code/spider/spider.py
@@ -5,6 +5,7 @@ from urllib import request
 class Spider():
     url = "http://jewelry.xbiao.com/"
     root_patten = '<div class="main-content">([\s\S]*?)</ul>'
+    li_patten = '<li>([\s\S]*?)</li>'
     name_patten = '<span>([\s\S]*?)</span>'
     price_patten = '<i>([\s\S]*?)</i>'
 
@@ -17,17 +18,27 @@ class Spider():
 
     def __analysiy(self, htmls):
         root_html = re.findall(Spider.root_patten, htmls)
+        li_htmls = []
         brands = []
         for html in root_html:
-            name = re.findall(Spider.name_patten, html)
-            price = re.findall(Spider.price_patten, html)
-            brand = {'name': name,'price': price}
+            li_htmls = re.findall(Spider.li_patten,html)
+
+
+        for li in li_htmls:
+            name = re.findall(Spider.name_patten,li)
+            price = re.findall(Spider.price_patten,li)
+            brand = {'name':name,'price':price}
             brands.append(brand)
         print(brands)
 
+
+
+
+
+
     def go(self):
         html = self.__fetch_contents()
-        sec_html = self.__analysiy(html)
+        self.__analysiy(html)
 
 
 result = Spider()
