9f588014047cdbecd19f9448d431d53f53c01507
david <785687000@qq.com>
2019-11-25 13:30:53 +0800

    字典代替switch语句

--- home-study/study-code/python-code/nine/one.py
+++ home-study/study-code/python-code/nine/one.py
@@ -1,1 +1,11 @@
-print('你好')
+#字典代替switch语句
+day = 6
+switcher = {
+    0:'sun',
+    1:'mon',
+    2:'tues'
+}
+
+switch = switcher.get(day,'unknow')
+
+print(switch)
--- home-study/study-code/python-code/spider/spider.py
+++ home-study/study-code/python-code/spider/spider.py
@@ -1,8 +1,15 @@
+'''
+模块注释
+'''
 import re
 from urllib import request
-
-
+#爬虫框架BeautifulSoup Scrapy
+#爬虫，反爬虫，反反爬虫
+#频繁爬取数据，IP被封，使用IP代理
 class Spider():
+    '''
+    类注释
+    '''
     url = "http://jewelry.xbiao.com/"  # 源网页
     root_patten = '<div class="main-content">([\s\S]*?)</ul>'  # 第一层剥壳
     li_patten = '<li>([\s\S]*?)</li>'  # 第二层剥壳
@@ -10,15 +17,24 @@ class Spider():
     price_patten = '<i>([\s\S]*?)</i>'  # 匹配价格
     hot_patten = '<em>([\d]*?)</em>'  # 匹配热度
 
-    # 获取网页内容
+    # 获取数据
     def __fetch_contents(self):
+        '''
+        方法注释
+        :return:html
+        '''
         r = request.urlopen(Spider.url)
         html = r.read()
         html = str(html, encoding='utf-8')
         return html
 
-    # 网页内容剥壳
+    # 数据剥壳
     def __analysiy(self, htmls):
+        '''
+
+        :param htmls:
+        :return: brands
+        '''
         root_html = re.findall(Spider.root_patten, htmls)
         li_htmls = []
         brands = []
@@ -33,8 +49,13 @@ class Spider():
             brands.append(brand)
         return brands
 
-    # 网页内容精简
+    # 数据精炼
     def __refine(self, brands):
+        '''
+
+        :param brands:
+        :return: ,str(brand(name)),str(brand(price)),str(brand(hot))
+        '''
         ld = lambda brand: {
             'name': brand['name'][0].strip(),
             'price': brand['price'][0],
@@ -42,8 +63,14 @@ class Spider():
         }
         return map(ld, brands)
 
-    # 排序
+
+    # 数据排序
     def __sort(self, brands):
+        '''
+
+        :param brands:
+        :return:brands
+        '''
         brands = sorted(brands, key=self.__sort_seed, reverse=True)
         return brands
 
@@ -53,9 +80,15 @@ class Spider():
         return hot
 
     # 展示
+    # def __show(self, brands):
+    #     for brand in brands:
+    #         print(brand['name'] + '-----' + brand['price'] + '----' + brand['hot'])
     def __show(self, brands):
-        for brand in brands:
-            print(brand['name'] + '-----' + brand['price'] + '----' + brand['hot'])
+        for rank in range(0, len(brands)):
+            print('rank ' + str(rank + 1)
+                  + ' name:' + brands[rank]['name']
+                  + ' price:' + brands[rank]['price']
+                  + ' hot:' + brands[rank]['hot'])
 
     # 入口
     def go(self):
