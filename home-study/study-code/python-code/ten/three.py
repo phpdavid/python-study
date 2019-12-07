0f85282a7c0cbc2ac2438ff40c5be1e1dc70b104
david <785687000@qq.com>
2019-12-07 17:45:48 +0800

    加注释

--- home-study/study-code/python-code/ten/three.py
+++ home-study/study-code/python-code/ten/three.py
@@ -2,7 +2,7 @@
 from collections import Counter
 import re
 
-txt = open('test').read()
-res = re.split('\W+', txt)
-counter = Counter(res)
-print(counter.most_common(5))
+txt = open('test').read()  # 读取文件
+res = re.split('\W+', txt)  # 正则分割
+counter = Counter(res)  # counter计数
+print(counter.most_common(5))  # 排序
